a1a23a175310ea165f894c921f4c2ac62e08ff12
nguyentruongtho <nguyentruongtho.sg@gmail.com>
2013-11-10 11:18:18 +0800

    Example how to handle link element

--- sample/src/main/java/com/truongtho/macd/sample/MacdActivity.java
+++ sample/src/main/java/com/truongtho/macd/sample/MacdActivity.java
@@ -1,14 +1,19 @@
 package com.truongtho.macd.sample;
 
 import android.app.Activity;
+import android.content.Intent;
+import android.net.Uri;
 import android.os.Bundle;
 import android.text.Html;
 import android.text.method.LinkMovementMethod;
+import android.util.Log;
+import android.view.View;
 import android.widget.TextView;
 import com.truongtho.macd.MacdTextView;
 
 public class MacdActivity extends Activity {
-  private MacdTextView macdTextView;
+
+  private static final String TAG = MacdActivity.class.getName();
 
   /** Called when the activity is first created. */
   @Override
@@ -16,6 +21,25 @@ public class MacdActivity extends Activity {
     super.onCreate(savedInstanceState);
     setContentView(R.layout.main);
 
-    macdTextView = (MacdTextView) findViewById(R.id.macd_text_view);
+    MacdTextView macdTextView = (MacdTextView) findViewById(R.id.macd_text_view);
+    macdTextView.setOnElementClickListener(createOnElementClickListener());
+  }
+
+  private MacdTextView.OnElementClickListener createOnElementClickListener() {
+    return new MacdTextView.OnElementClickListener() {
+      @Override public void onClick(View textView, String data, String key, String[] matchStrings) {
+        if (key.equals("link")) {
+          String text = matchStrings[1];
+          String url = matchStrings[2];
+          Log.d(TAG, String.format("Clicked on %s, opening url %s", text, url));
+
+          Intent i = new Intent(Intent.ACTION_VIEW);
+          i.setData(Uri.parse(url));
+          startActivity(i);
+        } else {
+          Log.d(TAG, "Element " + key + " has been clicked!");
+        }
+      }
+    };
   }
 }
